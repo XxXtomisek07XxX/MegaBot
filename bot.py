8be8fc16ca6c5d9ba612e29e5e2540ee1d2bcc2c
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-16 22:40:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import asyncio
 import platform
 import colorsys
 import random
-import os
+import os 
 import time
 
 import discord
@@ -17,30 +17,30 @@ import asyncio
 bot=commands.Bot(command_prefix='/')
 client.remove_command('help')
 
-@bot.event
+@client.event
 async def on_ready():
   print('The bot is ready!')
   print(bot.user.name)
   print(bot.user.id)
   
-@bot.command()
+@client.command()
 async def fakeban():
   await client.say('Dostal/a jsi ban! Ha jen si dělám standu :D')
   
-@bot.command()
+@client.command()
 async def support():
   await client.say('cms.megabot-support.webnode.cz')
 
-@bot.command()
+@clienr.command()
 async def discord():
   await client.say('https://discord.gg/9uer
 N5z')
 
-@bot.command()
+@client.command()
 async def hosting():
   await client.say('****Zatím Žádný!****')
   
-@bot.command()
+@client.command()
 async def discordbot():
   await client.say('https://discord.gg/8dbfhWU')
 
