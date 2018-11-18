ee0abe7607c923685fbf98566c8f3f0d346c713f
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-18 22:21:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
+  await client.change_presence(game=discord.Game(name= /help
   print('The bot is ready!')
   print(bot.user.name)
   print(bot.user.id)
