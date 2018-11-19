5604268da47b34d20793911502f0a7991a90aa4e
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-19 20:04:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -169,7 +169,7 @@ async def warn(ctx, *, member : discord.Member = None):
 @commands.has_permissions(kick_members=True)     
 async def userinfo(ctx, user: discord.Member):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color = discord.Color((r << 16) + (g << 8) + b))
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Hráčské Info.", color = discord.Color((r << 16) + (g << 8) + b))
     embed.add_field(name="Jméno", value=user.name, inline=True)
     embed.add_field(name="ID", value=user.id, inline=True)
     embed.add_field(name="Status", value=user.status, inline=True)
