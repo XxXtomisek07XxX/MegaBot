3a8affb259cefc22d304583d8568c35a55279f32
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2018-11-29 17:05:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,7 +87,7 @@ async def help():
 ```bans``` - list zabanovaných hráčů
 ```unmute <@hráč>``` - zruší u určeného (označeného hráče) mute
 
-Před každý příkaz dejte prefix, ```/```
+Před každý příkaz dejte prefix, ```/``` """)
         
         
         
