454d46b3b0ecb4753ba637c7e2f69803b10dcec8
XxXtomisek07XxX <42677459+XxXtomisek07XxX@users.noreply.github.com>
2019-06-23 18:22:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -377,7 +377,7 @@ async def say(ctx, *, msg = None):
 async def mute(ctx, member: discord.Member):
      if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
         role = discord.utils.get(member.server.roles, name='Muted')
-        await bot.add_roles(member, role)
+        await bot.add_roles(member, role) 
         embed=discord.Embed(title="User byl Mutnut!", description="{0} dostal/a jsi mute od {1}!".format(member, ctx.message.author), color=0xff00f6)
         await bot.say(embed = embed)
      else:
